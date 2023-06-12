9bbc8d968ef479a045bec5e14d5ff7d1210ea99f
Guilherme Giachellin <guilherme.giache@gmail.com>
2023-06-12 20:54:07 -0300

    Funçao para criar db e tabela

--- app/src/main/java/com/example/listadecompras/MainActivity.java
+++ app/src/main/java/com/example/listadecompras/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.listadecompras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SQLiteDatabase bancoDados;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        criarBancoDados();
+    }
+
+    public void criarBancoDados() {
+        try {
+            bancoDados = openOrCreateDatabase("listaDeComprasDb", MODE_PRIVATE, null);
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS produto(" + " id INTEGER PRIMARY KEY AUTOINCREMENT" +
+                               " , nome VARCHAR" +
+                               " , valor DOUBLE)");
+            bancoDados.close();
+        } catch (Exception e) {
+
+        }
     }
 }
