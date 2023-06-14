cd322e090681d7feb38a9b3888716f72bb7e64da
Guilherme Giachellin <guilherme.giache@gmail.com>
2023-06-13 21:58:04 -0300

    criada tabela de listas e tabela conexao

--- app/src/main/java/com/example/listadecompras/MainActivity.java
+++ app/src/main/java/com/example/listadecompras/MainActivity.java
@@ -18,12 +18,29 @@ public class MainActivity extends AppCompatActivity {
     public void criarBancoDados() {
         try {
             bancoDados = openOrCreateDatabase("listaDeComprasDb", MODE_PRIVATE, null);
+            //TABELA DE PRODUTO
             bancoDados.execSQL("CREATE TABLE IF NOT EXISTS produto(" + " id INTEGER PRIMARY KEY AUTOINCREMENT" +
                                " , nome VARCHAR" +
                                " , valor DOUBLE)");
+            //TABELA DE LISTA
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS lista(" + " id INTEGER PRIMARY KEY AUTOINCREMENT" +
+                    " , nome VARCHAR" +
+                    " , data DATE)");
+            //TABELA DE CONEXAO
+            bancoDados.execSQL("CREATE TABLE listaComprasProdutos(" +
+                    "   ID INTEGER PRIMARY KEY AUTOINCREMENT" +
+                    " , produto_id INTEGER " +
+                    " , lista_id INTEGER" +
+                    " , FOREIGN KEY (produto_id) REFERENCES produto(id)" +
+                    " , FOREIGN KEY (lista_id) REFERENCES lista(id)" +
+                    ");");
             bancoDados.close();
         } catch (Exception e) {
 
         }
+    }
+
+    public void inserirProdutos() {
+
     }
 }
